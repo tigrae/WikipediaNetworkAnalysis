d20df43e1c406c44605d3b5a8fd77461a81b11b9
Tilman Gräf <tilman.graef@gmail.com>
2023-07-12 19:13:50 +0200

    implement category_scraper.py

---
+++ category_scraper.py
@@ -0,0 +1,84 @@
+import requests
+from queue import Queue
+
+
+class Category:
+    def __init__(self, name, parent=None, subcategories=None):
+        self.name = name
+        self.parent = parent
+        self.subcategories = subcategories if subcategories else []
+
+    def add_subcategory(self, category_object):
+        self.subcategories.append(category_object)
+
+    def __str__(self):
+        return f"Category: {self.name}"
+
+
+def get_direct_subcategories(category):
+    url = "https://en.wikipedia.org/w/api.php"
+    params = {
+        "action": "query",
+        "format": "json",
+        "list": "categorymembers",
+        "cmtitle": f"Category:{category}",
+        "cmtype": "subcat",
+        "cmlimit": "max"
+    }
+
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        subcategories = [category['title'].replace("Category:", "") for category in data['query']['categorymembers']]
+        return subcategories
+    else:
+        print('Error retrieving direct subcategories:', response.status_code)
+        return None
+
+
+if __name__ == "__main__":
+
+    root_category = Category("Fields of mathematics")
+
+    # define queue for categories not yet explored and categories_done set. Put root category in queue.
+    categories_queue = Queue()
+    categories_queue.put(root_category)
+    # categories_done = set()
+    categories_seen = set()
+
+    i = 0
+
+    while not categories_queue.empty():
+        current_category = categories_queue.get()
+
+        # check if the current category has been processed before
+        if current_category.name in categories_seen:
+            continue
+
+        found_subcategories = get_direct_subcategories(current_category.name)
+
+        # create objects for found subcategories, add them to current_category subcategories and queue.
+        for category in found_subcategories:
+
+            new_class = Category(category, current_category.name)
+            current_category.add_subcategory(new_class)
+            categories_queue.put(new_class)
+
+        # categories_done.add(current_category)
+        categories_seen.add(current_category.name)
+
+        i += 1
+        if i % 10 == 0:
+            print(f"Progress after {i} iterations: Done = {len(categories_seen)}, in Queue = {categories_queue.qsize()}")
+
+
+    pass
+
+    # subcategories = get_direct_subcategories("Fields of mathematics")
+    # for c in subcategories:
+    #     print(c)
+    #
+    # print()
+    # subcategories = get_direct_subcategories(subcategories[0])
+    # for c in subcategories:
+    #     print(c)
--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import requests
+import re
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
 def url_request(url):
@@ -11,6 +13,73 @@ def url_request(url):
     return requests.get(url)
 
 
+def get_categories(article_url):
+
+    # Make a request to the article URL
+    response = url_request(article_url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content of the article
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find the categories section in the article
+        categories_section = soup.find(id='mw-normal-catlinks')
+
+        # Extract the category links
+        categories = []
+        if categories_section:
+            category_links = categories_section.find_all('a')
+            for link in category_links:
+                category = link.text
+                if category != 'Categories':
+                    categories.append(category)
+
+        return categories
+    else:
+        print("Error retrieving article:", response.status_code)
+        return None
+
+
+def extract_wikipedia_links(article_url):
+    # Make a request to the article URL
+    response = url_request(article_url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content of the article
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find all the anchor tags in the article
+        links = soup.find_all('a')
+
+        pass
+
+        # Extract the Wikipedia links
+        wikipedia_links = set()
+        for link in links:
+            href = link.get('href')
+            if href and re.match(r'^/wiki/[^:]+$', href):
+                wikipedia_links.add(href)
+
+        pass
+
+        # Remove the link to the page itself
+        wikipedia_links.discard(article_url.replace("https://en.wikipedia.org", ""))
+
+        return list(wikipedia_links)
+    else:
+        print('Error retrieving article:', response.status_code)
+        return None
+
+
+def check_wikipedia_article_exists(url):
+    if not url.startswith("https://en.wikipedia.org"):
+        url = f"https://en.wikipedia.org{url}"
+    response = requests.head(url)
+    return response.status_code == 200
+
+
 def get_title(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     title_element = soup.find('h1', id='firstHeading')
@@ -19,12 +88,20 @@ def get_title(html_content):
 
 if __name__ == "__main__":
 
-    url = "https://en.wikipedia.org/wiki/Mathematics"
-    response = requests.get(url)
-    html_content = response.text
+    root_url = "https://en.wikipedia.org/wiki/Mathematics"
 
-    title = get_title(html_content)
+    categories = get_categories(root_url)
+    links = extract_wikipedia_links(root_url)
 
+    if categories:
+        print(f"Categories: {categories}")
+
+    for link in links:
+        print(f"  {link}")
+
+    for link in links:
+        if not check_wikipedia_article_exists(link):
+            print(f"{link} not found")
 
 
 
