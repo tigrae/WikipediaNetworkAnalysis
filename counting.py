c55fb22b9e6bfca29eb5dc141d114814d526c030
Tilman Gräf <tilman.graef@gmail.com>
2023-08-31 17:49:51 +0200

    development on the visualization of results

--- counting.py
+++ counting.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from util.classes import *
 
 def count_articles_by_root_cats(articles):
@@ -27,6 +28,7 @@ def count_articles_by_related_cats(articles):
 
     return count_dict
 
+
 def count_relations(articles):
     num_relations = 0
     individual_relations = set()
@@ -34,7 +36,6 @@ def count_relations(articles):
         num_relations += len(article.relations)
         individual_relations.update(article.relations)
     print(f"{num_relations} Links leading to {len(individual_relations)} individual articles.")
-
 
 
 def count_sources(articles, prt=True):
@@ -55,6 +56,21 @@ def count_sources(articles, prt=True):
     return count_dict
 
 
+def count_cat_occurrences_in_articles(categories, articles, prt=True):
+
+    count_dict = {category.title: 0 for category in categories}
+
+    for article in tqdm.tqdm(articles, desc="Counting category occurrences in articles"):
+        for category in [c.title for c in categories]:
+            if category in article.related_cats:
+                count_dict[category] += 1
+
+    if prt:
+        for num_cat_occs, count in count_dict.items():
+            print(f"{num_cat_occs} occurrences: {count}")
+
+    return count_dict
+
 
 if __name__ == "__main__":
 
@@ -72,9 +88,28 @@ if __name__ == "__main__":
     # count_relations(articles)
 
     """ get number of related categories """
-    article_count_by_related_cats = count_articles_by_related_cats(articles)
-    for num_related_cats, count in article_count_by_related_cats.items():
-        print(f"Articles with {num_related_cats} related categories: {count}")
+    # article_count_by_related_cats = count_articles_by_related_cats(articles)
+    # for num_related_cats, count in article_count_by_related_cats.items():
+    #     print(f"Articles with {num_related_cats} related categories: {count}")
 
     """ get number of related categories """
     # article_count_by_related_cats = count_sources(articles, prt=True)
+
+    """ get number of category occurrences in articles """
+    ncaia = count_cat_occurrences_in_articles(categories, articles)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    bars = ax.barh(list(ncaia.keys())[::-1], list(ncaia.values())[::-1], color='blue')
+    plt.xlabel("Number of occurrences")
+    plt.ylabel("Category")
+    plt.title("Category Occurrences in Articles")
+    plt.tight_layout()
+
+    for bar in bars:
+        plt.text(bar.get_width(), bar.get_y() + bar.get_height() / 2, f'{bar.get_width():.0f}', ha='left',
+                 va='center', color='black')
+
+    ax.grid(axis='x', which='both', linestyle='dashed', linewidth=0.8, color='gray')
+
+    plt.show()
+
--- visualization.py
+++ visualization.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import tqdm
 from statistics import mean, stdev
-from util.classes import load_all_categories, Article
+from util.classes import load_all_categories, load_all_articles, Article
+from counting import count_cat_occurrences_in_articles
 
 
 def draw_horizontal_bar_chart(plot_dict, title="", xlabel="", ylabel=""):
     fig, ax = plt.subplots(figsize=(10, 6))
-    bars = ax.barh(list(plot_dict.keys())[::-1], list(plot_dict.values())[::-1], color='blue')
+    bars = ax.barh(list(plot_dict.keys())[::-1], list(plot_dict.values())[::-1], color="#00b87a")
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
@@ -32,7 +33,7 @@ def draw_category_article_number(categories, n=0):
 
     cat_article_count = {}
     for category in categories:
-        cat_article_count[category.title] = len(category.articles)
+        cat_article_count[category.title.replace("Category:", "").replace("_", " ")] = len(category.articles)
 
     # sort and get max name length
     cat_article_count = dict(sorted(cat_article_count.items(), key=lambda item: item[1], reverse=True))
@@ -47,35 +48,35 @@ def draw_category_article_number(categories, n=0):
 
     # draw_horizontal_bar_chart(cat_article_count, title="Number of Articles per Category", xlabel="Articles", ylabel="Category")
 
-    if n > 0:
 
-        loaded_articles = {}
-        related_article_count = {}
-        for category in categories:
-            related_article_count[category.title] = 0
-            for article_title in category.articles:
-                _current_article = None
-                if article_title not in loaded_articles:
-                    _current_article = Article(article_title, autosave=False)
-                    loaded_articles[article_title] = _current_article
-                else:
-                    _current_article = loaded_articles[article_title]
-                related_article_count[category.title] += len(_current_article.relations)
 
-        # sort and get max name length
-        # related_article_count = dict(sorted(related_article_count.items(), key=lambda item: item[1], reverse=True))
-        related_article_count = {key: related_article_count[key] for key in cat_article_count.keys()}
-        # related_article_count = dict(sorted(related_article_count.items(), key=lambda item: item[1], reverse=True))
-        max_name_length = max(map(len, related_article_count.keys()))
+    loaded_articles = {}
+    related_article_count = {}
+    for category in categories:
+        related_article_count[category.title.replace("Category:", "").replace("_", " ")] = 0
+        for article_title in category.articles:
+            _current_article = None
+            if article_title not in loaded_articles:
+                _current_article = Article(article_title, autosave=False)
+                loaded_articles[article_title] = _current_article
+            else:
+                _current_article = loaded_articles[article_title]
+            related_article_count[category.title.replace("Category:", "").replace("_", " ")] += len(_current_article.relations)
 
-        # print and draw
-        for category, num_articles in related_article_count.items():
-            print(f"\033[34;1m{category.ljust(max_name_length)}: {num_articles} Related Articles (1st degree)\033[31;0m")
-        print(f"-> Mean: {mean(related_article_count.values())}")
-        print(f"-> Standard Deviation: {stdev(related_article_count.values())}")
-        print()
+    # sort and get max name length
+    # related_article_count = dict(sorted(related_article_count.items(), key=lambda item: item[1], reverse=True))
+    related_article_count = {key: related_article_count[key] for key in cat_article_count.keys()}
+    # related_article_count = dict(sorted(related_article_count.items(), key=lambda item: item[1], reverse=True))
+    max_name_length = max(map(len, related_article_count.keys()))
 
-        # draw_horizontal_bar_chart(related_article_count, title="Number of related Articles", xlabel="Articles", ylabel="Category")
+    # print and draw
+    for category, num_articles in related_article_count.items():
+        print(f"\033[34;1m{category.ljust(max_name_length)}: {num_articles} Related Articles (1st degree)\033[31;0m")
+    print(f"-> Mean: {mean(related_article_count.values())}")
+    print(f"-> Standard Deviation: {stdev(related_article_count.values())}")
+    print()
+
+    # draw_horizontal_bar_chart(related_article_count, title="Number of related Articles", xlabel="Articles", ylabel="Category")
 
     """ representation as bar chart """
     # Create a figure with two subplots arranged side by side
@@ -91,6 +92,7 @@ def draw_category_article_number(categories, n=0):
                         color='green')
     axs[1].set_title('Indirectly Linked Articles')
     axs[1].set_xlabel('Number of Articles')
+    axs[1].set_yticklabels([])
 
     for i, bars in enumerate([bars1, bars2]):
         for bar in bars:
@@ -105,11 +107,20 @@ def draw_category_article_number(categories, n=0):
     # Show the combined figure with both bar charts
     plt.show()
 
+    if n == 2:
+        cat_occurrences = count_cat_occurrences_in_articles(categories, articles)
+        cat_occurrences_short_title = {key.replace("Category:", "").replace("_", " "): value for key, value in cat_occurrences.items()}
+        cat_occurrences_short_title = dict(sorted(cat_occurrences_short_title.items(), key=lambda item: item[1], reverse=True))
+        draw_horizontal_bar_chart(cat_occurrences_short_title, "Category Occurrences in Articles", "Number of occurrences")
+        print(f"-> Mean: {mean(cat_occurrences_short_title.values())}")
+        print(f"-> Standard Deviation: {stdev(cat_occurrences_short_title.values())}")
+
+
 if __name__ == "__main__":
 
     categories = load_all_categories("./saved/categories", autosave=False)
-    draw_category_article_number(categories, n=1)
-
+    articles = load_all_articles("./saved/articles", autosave=False)
+    draw_category_article_number(categories, n=2)
 
 
     pass
